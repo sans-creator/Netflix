ed7efdf35c03f569f2d45114c029ad063fd6cb3f
sans-creator <sanskarj163@gmail.com>
2025-07-21 01:02:07 +0530

    handlingScroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,35 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import "./Navbar.css"
 import logo from "../../assets/logo.png"
 import search_icon from "../../assets/search_icon.svg"
 import bell_icon from "../../assets/bell_icon.svg"
 import profile_img from "../../assets/profile_img.png"
 import caret_icon from "../../assets/caret_icon.svg"
+import { logout } from '../../firebase'
 
 
 const Navbar = () => {
+  const navRef=useRef()
+
+  useEffect(() => {
+  const handleScroll = () => {
+    if (window.scrollY >= 80) {
+      navRef.current.classList.add("navbar-dark");
+    } else {
+      navRef.current.classList.remove("navbar-dark");
+    }
+  };
+
+  window.addEventListener("scroll", handleScroll);
+
+  return () => {
+    window.removeEventListener("scroll", handleScroll); // Clean up
+  };
+}, []);
+
+
   return (
-    <div className='navbar'>
+    <div ref={navRef} className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt="" />
         <ul>
@@ -30,7 +50,7 @@ const Navbar = () => {
           <img src={caret_icon} alt=""  />
 
           <div className="dropdown">
-            <p>Sign Out Of Netflix</p>
+            <p onClick={()=>{logout()}}>Sign Out Of Netflix</p>
           </div>
 
         </div>
