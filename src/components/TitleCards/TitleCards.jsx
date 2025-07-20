95dc3b3d4c3e05a6ab6305b3c9f395f451ddb11e
sans-creator <sanskarj163@gmail.com>
2025-07-21 01:02:41 +0530

    tmbd

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,33 +1,48 @@
-import React, { useEffect, useRef } from 'react';
-import "./TitleCards.css";
-import cards_data from "../../assets/cards/Cards_data";
+import React, { useEffect, useRef, useState } from 'react';
+import './TitleCards.css';
+import { Link } from 'react-router-dom';
 
+const TitleCards = ({ title, category }) => {
+  const [apiData, setApiData] = useState([]);
+  const cardsRef = useRef();
 
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization:
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ODQ3MmUyMTI5ZDkyNWY1M2MwODUyOTJiOGQxZWY2MiIsIm5iZiI6MTc1MzAyOTIwNy4yMTksInN1YiI6IjY4N2QxYTU3NTg1NTk3YzEwMGY3NmEzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.T1j1Oc30jA3EQv7cu_CKSaQ6mJieE8otTThipKPXpDw'
+    }
+  };
 
-
-const TitleCards = ({title,category}) => {
-  const cardsRef=useRef();
-
-  const handleWheel=(e)=>{
+  const handleWheel = (e) => {
     e.preventDefault();
-    cardsRef.current.scrollLeft+=e.deltaY;
-}
+    cardsRef.current.scrollLeft += e.deltaY;
+  };
 
- useEffect(() => {
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category?category:"now_playing"}?language=en-US&page=1`, options)
+      .then((res) => res.json())
+      .then((res) => setApiData(res.results))
+      .catch((err) => console.error(err));
+
     const current = cardsRef.current;
-    current.addEventListener("wheel", handleWheel);
-    return () => current.removeEventListener("wheel", handleWheel);
-  }, []);
+    current.addEventListener('wheel', handleWheel);
+    return () => current.removeEventListener('wheel', handleWheel);
+  }, [category]);
+
   return (
-    
-    <div className='titlecards'>
-      <h2>{title?title:"Popular on Netflix"}</h2>
+    <div className="titlecards">
+      <h2>{title ? title : 'Popular on Netflix'}</h2>
       <div className="card-list" ref={cardsRef}>
-        {cards_data.map((card, index) => (
-          <div className="card" key={index}>
-            <img src={card.image} alt={card.name} />
-            <p>{card.name}</p>
-          </div>
+        {apiData.map((card, index) => (
+          <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${card.backdrop_path || card.poster_path}`}
+              alt={card.original_title || card.name}
+            />
+            <p>{card.original_title || card.name}</p>
+          </Link>
         ))}
       </div>
     </div>
